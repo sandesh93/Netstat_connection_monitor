3e39fd4707084b9b83a4b09601de517c261c6bb5
Sandesh_Rane <sandeshrane93@gmail.com>
2020-06-04 15:57:44 +0530

    added mtime and mmin

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
-#!/bin/bash                                                
-                                                           
-BASE_PATH="/scripts/monitoring/netstat"                    
-TIME=$(date +%F.%T | tr ":" "_")                           
+#!/bin/bash
 
-#Clear log files older than 2 days 
-find /var/log -name "*.log" -type f -mtime +3 -exec rm -f {} \;
+BASE_PATH="/scripts/monitoring/netstat"
+TIME=$(date +%F.%T | tr ":" "_")
 
-#Capture netstat connections                                                           
-netstat -nautp | grep "LISTEN" > $BASE_PATH/file-$TIME.log 
+#Clear log files older than 2 days
+find $BASE_PATH -name "*.log" -type f -mmin +59 -exec rm -f {} \;    ### for files older than x min
+
+find $BASE_PATH -name "*.log" -type f -mtime +3 -exec rm -f {} \;    ### for files older than x days
+
+#Capture netstat connections
+netstat -nautp | grep "LISTEN" > $BASE_PATH/file-$TIME.log
